cd543265b9389c5bbbf740e666bf9d96b3a08f9f
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-25 15:17:55 -0400

    Implements ControlledComponent pattern in NewMessageForm

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -2,9 +2,26 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 
 class NewMessageForm extends React.Component {
+	state = {
+		message: '',
+		isDisabled: true
+	}
+	onInputChange = (input) => {
+		this.setState(()=>({
+			message: input
+		}))
+	}
 	render() {
+		const {isDisabled, message} = this.state
 		return(
-			<form ></form>
+			<form className="input-group">
+                <input type="text" className="form-control" placeholder="Enter your message..." value={message} onChange={(event)=>this.onInputChange(event.target.value)}  />
+                <div className="input-group-append">
+	              <button className="btn submit-button" disabled={isDisabled}>
+	                SEND
+	              </button>
+                </div>
+            </form>
 		)
 	}
 }
