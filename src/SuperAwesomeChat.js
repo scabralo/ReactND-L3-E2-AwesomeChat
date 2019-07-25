5e995652f93b17e3ee06c168be3361935003c941
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-25 13:57:16 -0400

    Adds SuperAwesomeChat component to project

---
+++ src/SuperAwesomeChat.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class SuperAwesomeChat extends React.Component {
+	render() {
+		return(
+			<div className='chat-window'>
+				<h2>Super Awesome Chat</h2>
+				
+			</div>
+		)
+	}
+}
+
+export default SuperAwesomeChat
