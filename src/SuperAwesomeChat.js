dd7807121cbeccf29368b598228411069eea4835
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-25 15:16:22 -0400

    Implements NewMessageForm within SuperAwesomeChat

--- src/SuperAwesomeChat.js
+++ src/SuperAwesomeChat.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import CurrentUser from './CurrentUser'
 import MessagesList from './MessagesList'
+import NewMessageForm from './NewMessageForm'
 
 class SuperAwesomeChat extends React.Component {
 	render() {
@@ -11,6 +12,7 @@ class SuperAwesomeChat extends React.Component {
 				<h2>Super Awesome Chat</h2>
 				<CurrentUser userName={user.username} />
 				<MessagesList messages={messages} username={user.username} />
+				<NewMessageForm />
 			</div>
 		)
 	}
