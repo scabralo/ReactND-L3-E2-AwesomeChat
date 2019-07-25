7b4ac1005d7b4696708f340d4f52495396b9afe1
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-25 14:29:49 -0400

    Updates prop from user.username to user

--- src/SuperAwesomeChat.js
+++ src/SuperAwesomeChat.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import CurrentUser from './CurrentUser'
+import MessagesList from './MessagesList'
 
 class SuperAwesomeChat extends React.Component {
 	render() {
-		const {user} = this.props
+		const {user, messages} = this.props
 		return(
 			<div className='chat-window'>
 				<h2>Super Awesome Chat</h2>
-				<CurrentUser user={user} />
+				<CurrentUser userName={user.username} />
+				<MessagesList messages={messages} userName={user.username} />
 			</div>
 		)
 	}
+}
+
+SuperAwesomeChat.propTypes = {
+	user: PropTypes.object.isRequired,
+	messages: PropTypes.array.isRequired
 }
 
 export default SuperAwesomeChat
