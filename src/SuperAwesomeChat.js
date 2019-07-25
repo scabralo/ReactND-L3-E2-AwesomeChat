6b1e15b407cbe01b721128346a5331131bee10e9
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-25 14:17:43 -0400

    Implements CurrentUser within SuperAwesomeChat

--- src/SuperAwesomeChat.js
+++ src/SuperAwesomeChat.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import CurrentUser from './CurrentUser'
 
 class SuperAwesomeChat extends React.Component {
 	render() {
+		const {user} = this.props
 		return(
 			<div className='chat-window'>
 				<h2>Super Awesome Chat</h2>
-				
+				<CurrentUser user={user} />
 			</div>
 		)
 	}
